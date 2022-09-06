6c5509c8dc0f6fe9f26e778b1f6cf4173d30dc7b
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-06 17:18:21 -0400

    Time: 379 ms (65.91%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -3,12 +3,10 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-      SELECT salary
-      FROM (
-           select salary, 
+      SELECT salary FROM (
+           select DISTINCT salary, 
            dense_rank()over( order by salary desc) ranking
            From employee) s
            WHERE ranking = N
-           LIMIT 1
   ); 
 END
