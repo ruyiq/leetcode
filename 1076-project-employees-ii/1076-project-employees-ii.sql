e99f8042d40bc529363e827f9d27ae2af3d3fbdf
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-08 13:05:38 -0400

    Time: 997 ms (44.73%), Space: 0B (100.00%) - LeetHub

--- 1076-project-employees-ii/1076-project-employees-ii.sql
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
 
-WITH EmployeeCount AS (
-    SELECT project_id, COUNT(DISTINCT employee_id) AS counts
-    FROM Project    
+WITH cte AS (
+    SELECT project_id, COUNT(DISTINCT employee_id) AS total
+    FROM Project
     GROUP BY project_id
-    ORDER BY counts DESC
 )
-SELECT project_id
-FROM EmployeeCount
-WHERE counts = (
-    SELECT MAX(counts)
-    FROM EmployeeCount
-)
+
+SELECT DISTINCT project_id  
+FROM cte
+WHERE total = (SELECT MAX(total) FROM cte)
