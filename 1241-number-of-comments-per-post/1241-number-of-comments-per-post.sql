9b479585cb3a635c26bf1dd8ece19b2be284b311
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-08 19:19:34 -0400

    Time: 1584 ms (56.30%), Space: 0B (100.00%) - LeetHub

---
+++ 1241-number-of-comments-per-post/1241-number-of-comments-per-post.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT s1.sub_id AS post_id,
+    IFNULL(COUNT(DISTINCT s2.sub_id),0) AS number_of_comments
+FROM Submissions s1
+LEFT JOIN Submissions s2
+    ON s1.sub_id =s2.parent_id 
+WHERE s1.parent_id IS NULL
+GROUP BY
+    post_id
+ORDER BY
+    post_id
