c69849910c961e4ac2080ff4ffecb13732d3674e
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-10 15:20:48 -0400

    Time: 2204 ms (6.71%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,6 +1,6 @@
-
-    SELECT DISTINCT product_id, p.product_name
-    FROM Sales
-    JOIN Product p
-        USING(product_id)
-    WHERE QUARTER(sale_date)=1 AND product_id NOT IN (SELECT product_id FROM Sales WHERE QUARTER(sale_date) IN (2,3,4));
+SELECT DISTINCT product_id, product_name
+FROM Product p
+JOIN Sales s
+    USING(product_id)
+WHERE product_id NOT IN (SELECT product_id FROM Sales WHERE QUARTER(sale_date) IN (2,3,4))
+    AND YEAR(s.sale_date)=2019;
