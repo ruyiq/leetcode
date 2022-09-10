3503f02ff7ccbc1a33cdcc5c96b5e8b2716577fb
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-10 19:54:47 -0400

    Time: 1016 ms (22.58%), Space: 0B (100.00%) - LeetHub

---
+++ 1294-weather-type-in-each-country/1294-weather-type-in-each-country.sql
@@ -0,0 +1,8 @@
+SELECT c.country_name, 
+    CASE WHEN AVG(w.weather_state)<=15 THEN "Cold"
+        WHEN AVG(w.weather_state)>=25 THEN "Hot"
+        ELSE "Warm" END AS weather_type 
+FROM Countries c
+JOIN Weather w
+    ON c.country_id = w.country_id AND MONTH(day)=11 AND YEAR(day)=2019
+GROUP BY c.country_id;
