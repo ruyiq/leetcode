e7494a51b1491f1263d26bf14e7984e71fdfc44b
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-11 19:30:51 -0400

    Time: 861 ms (15.15%), Space: 0B (100.00%) - LeetHub

--- 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -1,6 +1,8 @@
+WITH count_scheduled AS (
+    SELECT COUNT(delivery_id) as c
+    FROM Delivery
+    WHERE order_date=customer_pref_delivery_date)
 
-WITH cte AS (
-    SELECT COUNT(DISTINCT delivery_id) AS t FROM Delivery WHERE order_date=customer_pref_delivery_date)
-    
-SELECT ROUND(cte.t/COUNT(d.delivery_id)*100,2) AS immediate_percentage 
-FROM Delivery d, cte;
+SELECT ROUND(count_scheduled.c/COUNT(d.delivery_id)*100,2) AS immediate_percentage
+FROM Delivery d, count_scheduled;
+
