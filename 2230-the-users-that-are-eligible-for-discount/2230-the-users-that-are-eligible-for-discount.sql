92b63f475aff5afc111652fc02e58e04c61f6730
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-13 11:58:43 -0400

    Time: 1220 ms (10.71%), Space: 0B (100.00%) - LeetHub

---
+++ 2230-the-users-that-are-eligible-for-discount/2230-the-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+BEGIN
+	SELECT DISTINCT user_id 
+    FROM Purchases 
+    WHERE time_stamp>=startDate AND time_stamp<=endDate AND amount>=minAmount
+	ORDER BY user_id;
+END
