7e06afbf57ad38a4462b7037cd3f23e00e39d1e0
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-13 11:42:56 -0400

    Time: 421 ms (72.88%), Space: 0B (100.00%) - LeetHub

--- 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
+++ 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(DISTINCT customer_id) AS rich_count
 FROM Store 
-WHERE customer_id IN (SELECT DISTINCT customer_id FROM Store WHERE amount>500);
+WHERE customer_id IN (SELECT customer_id FROM Store WHERE amount>500);
