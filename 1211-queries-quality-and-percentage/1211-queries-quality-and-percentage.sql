1bc09f31be4aa88501b07a426443de956960de5a
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-12 21:05:02 -0400

    Time: 700 ms (69.47%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,5 +1,5 @@
-SELECT query_name, 
-    ROUND(AVG(rating /position ),2) AS quality ,
-    ROUND(SUM(IF(rating<3,1,0))/COUNT(rating)*100,2) AS poor_query_percentage 
+SELECT query_name,
+    ROUND(SUM(rating/position)/COUNT(query_name),2) AS quality,
+    ROUND(SUM(IF(rating<3,1,0))/COUNT(*)*100,2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
