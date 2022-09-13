39f06ffb95d8b6b038470430b824ffdcfca4d04a
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-13 19:46:34 -0400

    Time: 2018 ms (8.68%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -3,6 +3,4 @@ SELECT s.product_id,
     s.quantity,
     s.price
 FROM (SELECT *, rank() over (PARTITION BY product_id ORDER BY year) as ranking FROM Sales) s
-JOIN Product p
-    USING(product_id)
 WHERE s.ranking=1;
