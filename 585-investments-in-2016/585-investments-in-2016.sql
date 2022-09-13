99f40fdf9f45049b45dfd43a17e5d2017ac2ee5c
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-13 17:27:27 -0400

    Time: 1021 ms (11.90%), Space: 0B (100.00%) - LeetHub

---
+++ 585-investments-in-2016/585-investments-in-2016.sql
@@ -0,0 +1,4 @@
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE pid IN (SELECT pid FROM Insurance GROUP BY lat, lon HAVING COUNT(*) = 1)
+  AND tiv_2015 IN (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*) != 1)
