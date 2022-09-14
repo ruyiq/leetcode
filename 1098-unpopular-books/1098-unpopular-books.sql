d8d674ef282f08cbb2d668ff142afcda5f6ca041
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-13 20:27:19 -0400

    Time: 1417 ms (16.94%), Space: 0B (100.00%) - LeetHub

--- 1098-unpopular-books/1098-unpopular-books.sql
+++ 1098-unpopular-books/1098-unpopular-books.sql
@@ -1,15 +1,22 @@
-SELECT book_id, b.name               
-FROM Orders o
-JOIN Books b
-    USING(book_id)
-WHERE b.available_from <= date_sub('2019-06-23', interval 1 month) and o.dispatch_date between date_sub('2019-06-23', interval 1 year) and '2019-06-23'
-GROUP BY book_id 
-HAVING SUM(quantity)<10
+select
+    o.book_id,
+    b.name
+from Orders as o
+left join Books as b on o.book_id = b.book_id
+where b.available_from <= date_sub('2019-06-23', interval 1 month) 
+    and o.dispatch_date between date_sub('2019-06-23', interval 1 year) and '2019-06-23'
+group by o.book_id, b.name
+having sum(o.quantity) < 10
 
-UNION
+union
 
-SELECT book_id, name               
-FROM Books
-LEFT JOIN Orders o
-    USING(book_id)
-WHERE book_id NOT IN (SELECT book_id FROM Orders WHERE dispatch_date between date_sub('2019-06-23', interval 1 year) and '2019-06-23') 
+select
+    book_id,
+    name
+from Books
+where available_from <= date_sub('2019-06-23', interval 1 month) and book_id not in 
+(
+    select book_id 
+    from Orders 
+    where dispatch_date between date_sub('2019-06-23', interval 1 year) and '2019-06-23'
+)
