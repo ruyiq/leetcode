afb1373be0eee169de4f13a2afbfccb43db22f2f
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-16 22:28:36 -0400

    Time: 404 ms (20.33%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,4 +1,3 @@
-SELECT CASE WHEN COUNT(*)>0 THEN s.salary
-            ELSE null END AS SecondHighestSalary  
-FROM (SELECT dense_rank() over (ORDER BY salary DESC) AS ranking, salary FROM Employee) s
-WHERE s.ranking = 2;
+SELECT IF(COUNT(*)=0, null, salary) AS SecondHighestSalary
+FROM (SELECT *,dense_rank() over (ORDER BY salary DESC) ranking FROM Employee) s
+WHERE s.ranking=2;
