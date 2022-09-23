4d3e9df69141b430abc1e910f95a2bfa00e84f13
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 15:43:20 -0400

    Time: 1011 ms (15.01%), Space: 0B (100.00%) - LeetHub

--- 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
+++ 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
@@ -1,3 +1,3 @@
-SELECT s.player_id, s.device_id 
-FROM (SELECT rank() over(PARTITION BY player_id ORDER BY event_date) ranking, player_id, device_id FROM Activity) s 
-WHERE s.ranking=1;
+SELECT player_id, device_id 
+FROM (SELECT *, rank() over (PARTITION BY player_id ORDER BY event_date) ranking FROM Activity) s
+WHERE ranking=1;
