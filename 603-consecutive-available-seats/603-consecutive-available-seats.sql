42cffd87d40c28f81a83ea4353744b964d97b76b
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 16:24:14 -0400

    Time: 555 ms (31.18%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,5 +1,10 @@
-# Write your MySQL query statement below
-
-SELECT DISTINCT c2.seat_id
-FROM Cinema c1, Cinema c2
-WHERE abs(c1.seat_id-c2.seat_id)=1 AND c1.free=1 AND c2.free=1;
+SELECT seat_id 
+FROM Cinema 
+WHERE free=1 AND (seat_id+1 IN (SELECT seat_id 
+    FROM Cinema 
+    WHERE free=1 
+    ORDER BY seat_id )OR seat_id-1 IN (SELECT seat_id 
+    FROM Cinema 
+    WHERE free=1 
+    ORDER BY seat_id ))
+ORDER BY seat_id 
