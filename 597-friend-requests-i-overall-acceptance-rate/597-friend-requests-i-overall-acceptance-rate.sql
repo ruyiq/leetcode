77e3b1bf022426e69ac39a4791905de1670128dd
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 16:07:26 -0400

    Time: 1044 ms (9.74%), Space: 0B (100.00%) - LeetHub

--- 597-friend-requests-i-overall-acceptance-rate/597-friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/597-friend-requests-i-overall-acceptance-rate.sql
@@ -1,5 +1,2 @@
-with 
-cte1 as (select count(distinct requester_id, accepter_id) as cnt1 from RequestAccepted),
-cte2 as (select count(distinct sender_id, send_to_id) as cnt2 from FriendRequest)
-
-select if(cnt2 = 0, 0.00, round(cnt1/cnt2, 2)) as accept_rate from cte1, cte2;
+SELECT IFNULL(ROUND(COUNT(DISTINCT b.requester_id, b.accepter_id  )/COUNT(DISTINCT a.sender_id, a.send_to_id ),2),0) AS accept_rate 
+FROM FriendRequest a, RequestAccepted b;
