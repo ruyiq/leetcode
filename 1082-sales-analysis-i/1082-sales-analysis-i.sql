4d25f36c0f84acac200d5338713ca69b4b1a5d81
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 23:20:10 -0400

    Time: 1785 ms (20.48%), Space: 0B (100.00%) - LeetHub

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -1,15 +1,11 @@
-# Write your MySQL query statement below
-
-WITH CTE AS
-(
-        SELECT
-            seller_id,
-            DENSE_RANK() OVER(ORDER BY SUM(unit_price*quantity) DESC) as rnk
-        FROM Sales S
-        JOIN Product P
-        ON S.product_id = P.product_id
-        GROUP BY 1
+with cte as (
+    SELECT *, SUM(p.unit_price*s.quantity) AS p
+    FROM Product p
+    JOIN Sales s
+        USING(product_id)
+    GROUP BY seller_id
 )
-SELECT seller_id
-FROM CTE
-where rnk = 1
+
+SELECT seller_id   
+FROM cte
+WHERE p=(SELECT MAX(p) FROM cte);
