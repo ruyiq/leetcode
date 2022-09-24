3066cc56306731429c4abf18a6ebcf4c9e7dfa61
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 23:15:26 -0400

    Time: 1486 ms (12.32%), Space: 0B (100.00%) - LeetHub

--- 1076-project-employees-ii/1076-project-employees-ii.sql
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -1,8 +1,9 @@
-# Write your MySQL query statement below
-
 with cte as (
-    SELECT project_id,COUNT(employee_id) as total FROM Project GROUP BY project_id
+    SELECT project_id, COUNT(employee_id) as c
+    FROM Project
+    GROUP BY project_id
 )
+
 SELECT project_id
 FROM cte
-WHERE total = (SELECT MAX(total) FROM cte) ;
+WHERE c=(SELECT MAX(c) FROM cte)
