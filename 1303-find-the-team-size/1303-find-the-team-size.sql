c7a30478ce0cef2f217113e1146809adfd49d497
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 16:55:09 -0400

    Time: 298 ms (67.59%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,10 +1,10 @@
-WITH cte AS (
-    SELECT employee_id, team_id, COUNT(team_id) AS team_size  
-    FROM Employee 
-    GROUP BY team_id  
+with cte as (
+    SELECT team_id, COUNT(employee_id) AS team_size
+    FROM Employee
+    GROUP BY team_id 
 )
 
-SELECT e.employee_id, cte.team_size  
+SELECT e.employee_id, c.team_size
 FROM Employee e
-JOIN cte 
-    USING(team_id);
+JOIN cte c
+    USING(team_id)
