46df16c8700cdbd1a52fcfbca8ea1b9cdf15b0f2
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 22:41:46 -0400

    Time: 327 ms (42.00%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-Update salary
-SET sex = CASE
-    WHEN sex='m' THEN 'f'
-    ELSE 'm' END;
+UPDATE salary
+SET
+    sex = CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END;
