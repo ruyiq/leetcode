d778a8e0f31a60ade5c1214529289519d6f3696e
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 13:46:22 -0400

    Time: 729 ms (55.61%), Space: 0B (100.00%) - LeetHub

--- 1113-reported-posts/1113-reported-posts.sql
+++ 1113-reported-posts/1113-reported-posts.sql
@@ -1,5 +1,5 @@
-SELECT extra AS report_reason, COUNT(DISTINCT post_id) AS report_count 
-FROM Actions 
-WHERE action_date= "2019-07-04" AND action="report" 
+SELECT extra AS report_reason, COUNT(DISTINCT post_id) AS report_count
+FROM Actions
+WHERE action_date="2019-07-04" AND action="report"
 GROUP BY extra
-HAVING extra IS NOT NULL ;
+HAVING extra iS NOT null;
