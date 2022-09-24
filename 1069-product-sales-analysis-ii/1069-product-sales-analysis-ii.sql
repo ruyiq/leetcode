eee2512b2bdcfb77638e23f457179a5201c49ef1
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-23 22:46:35 -0400

    Time: 1464 ms (55.88%), Space: 0B (100.00%) - LeetHub

--- 1069-product-sales-analysis-ii/1069-product-sales-analysis-ii.sql
+++ 1069-product-sales-analysis-ii/1069-product-sales-analysis-ii.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-SELECT s.product_id, 
-    SUM(quantity) AS total_quantity
+SELECT s.product_id, SUM(s.quantity) AS total_quantity 
 FROM Sales s
 JOIN Product p
     USING(product_id)
