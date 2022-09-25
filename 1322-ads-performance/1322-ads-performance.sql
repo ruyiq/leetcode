f69e9daf6ab0103e76b84c5752713d10e8f04220
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 20:01:43 -0400

    Time: 997 ms (19.29%), Space: 0B (100.00%) - LeetHub

--- 1322-ads-performance/1322-ads-performance.sql
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -1,13 +1,6 @@
-with cte as (
-select ad_id, 
-       sum(case when action = 'Clicked' then 1 else 0 end) as "Clicked",
-       sum(case when action = 'Viewed' then 1 else 0 end) as "Viewed"
-from ads
-group by ad_id 
-)
-
-select ad_id,
-      round(ifnull((Clicked / (Clicked + Viewed))*100,0.00),2) as ctr
-from cte
-group by ad_id
-order by ctr desc, ad_id
+SELECT ad_id, 
+    CASE WHEN SUM(IF(action="Clicked",1,0))+SUM(IF(action="Viewed  ",1,0))=0 THEN 0
+    ELSE    ROUND(SUM(IF(action="Clicked",1,0))/(SUM(IF(action="Clicked",1,0))+SUM(IF(action="Viewed",1,0)))*100,2) END AS ctr
+FROM Ads 
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id;
