8e47069e1469283f17691992361065ed9d1bad66
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 21:35:12 -0400

    Time: 1109 ms (37.04%), Space: 0B (100.00%) - LeetHub

--- 1511-customer-order-frequency/1511-customer-order-frequency.sql
+++ 1511-customer-order-frequency/1511-customer-order-frequency.sql
@@ -1,24 +1,11 @@
-SELECT
-    C.customer_id,
-    C.name
-FROM
-    Customers AS C
-WHERE
-    C.customer_id IN
-        (SELECT
-            O.customer_id
-         FROM
-            Orders AS O
-         JOIN
-            Product AS P
-         USING
-            (product_id)
-         WHERE
-            YEAR(order_date) = '2020'
-         GROUP BY
-            O.customer_id
-         HAVING
-            SUM(IF(MONTH(order_date) = '06',price,0)*quantity) >= 100
-            AND
-            SUM(IF(MONTH(order_date) = '07',price,0)*quantity) >= 100
-        )
+SELECT c.customer_id, c.name
+FROM Customers c
+JOIN Orders o
+    ON o.customer_id=c.customer_id
+JOIN Product p
+    USING(product_id)
+GROUP BY c.customer_id  
+HAVING
+    SUM(IF(MONTH(order_date) = '06',price,0)*quantity) >= 100
+    AND
+    SUM(IF(MONTH(order_date) = '07',price,0)*quantity) >= 100
