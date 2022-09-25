c29681dc7e6462026b2c76fe152dd817f508df73
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 20:46:01 -0400

    Time: 1559 ms (22.09%), Space: 0B (100.00%) - LeetHub

--- 1421-npv-queries/1421-npv-queries.sql
+++ 1421-npv-queries/1421-npv-queries.sql
@@ -1,4 +1,4 @@
 SELECT q.id, q.year, IFNULL(n.npv,0) AS npv
-FROM NPV n
-RIGHT JOIN Queries q
-    ON n.id=q.id AND n.year=q.year
+FROM Queries q
+LEFT JOIN NPV n
+    ON q.id=n.id AND q.year=n.year
