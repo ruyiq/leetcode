614a6b4b2b403f8d6bcc89381258ed206dfafb13
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-24 21:09:18 -0400

    Time: 597 ms (33.20%), Space: 0B (100.00%) - LeetHub

--- 1435-create-a-session-bar-chart/1435-create-a-session-bar-chart.sql
+++ 1435-create-a-session-bar-chart/1435-create-a-session-bar-chart.sql
@@ -1,19 +1,20 @@
-
-select
-'[0-5>' as bin, count(duration /60) total
-from sessions where duration /60 <5
-
+with bins as (
+select '[0-5>' as bin
 union all
-select
-'[5-10>' as bin, count(duration /60) total
-from sessions where duration /60 >= 5 and duration /60 <10
-
+select '[5-10>' as bin
 union all
-select
-'[10-15>' as bin, ifnull(count(duration /60),0) total
-from sessions where duration /60 >=10 and duration /60 <15
-
+select '[10-15>' as bin
 union all
-select
-'15 or more' as bin, count(duration /60) total
-from sessions where duration /60 >=15
+select '15 or more' as bin
+), cte as(
+    SELECT CASE WHEN duration>=0 AND duration<5*60 THEN "[0-5>"
+                WHEN duration>=5*60 AND duration<10*60 THEN "[5-10>"
+                WHEN duration>=600 AND duration<15*60 THEN "[10-15>"
+                ELSE "15 or more" END AS bin, IFNULL(COUNT(duration),0) AS total
+    FROM Sessions
+    GROUP BY bin
+)
+SELECT bin, IFNULL(total,0) AS total        
+FROM cte
+RIGHT JOIN bins
+    USING(bin)
