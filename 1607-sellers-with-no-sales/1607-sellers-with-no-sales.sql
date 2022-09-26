a61a12c426170e1290c3e2be52f47ef1dad34f96
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-25 20:48:44 -0400

    Time: 2028 ms (6.05%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,8 +1,4 @@
-SELECT seller_name
-FROM Seller s
-LEFT JOIN Orders o
-    ON o.seller_id=s.seller_id 
-LEFT JOIN Customer c
-    ON o.customer_id=c.customer_id
-WHERE s.seller_id NOT IN (SELECT seller_id FROM Orders WHERE YEAR(sale_date)=2020)
+SELECT seller_name 
+FROM Seller
+WHERE seller_id NOT In (SELECT seller_id FROM Orders  WHERE YEAR(sale_date)=2020)
 ORDER BY seller_name;
