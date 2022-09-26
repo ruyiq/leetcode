c35dc7c7ca5494783deb50cf1b16739348f1f019
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-25 20:23:01 -0400

    Time: 1323 ms (7.08%), Space: 0B (100.00%) - LeetHub

--- 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
+++ 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
@@ -1,4 +1,6 @@
-SELECT LEFT(order_date, 7) AS month, COUNT(DISTINCT order_date, customer_id) AS order_count, COUNT(DISTINCT customer_id) AS customer_count
-FROM orders
+SELECT LEFT(order_date,7) AS month, 
+    COUNT(DISTINCT order_id) AS order_count,
+    COUNT(DISTINCT customer_id) AS customer_count 
+FROM Orders
 WHERE invoice>20
-GROUP BY month;
+GROUP BY month
