0b5bf7c902c783657a87f0c8c06147d370f68d08
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-26 23:05:44 -0400

    Time: 1421 ms (37.48%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,6 @@
-SELECT contest_id, 
-    ROUND(COUNT(*)/(SELECT COUNT(user_id) FROM Users)*100,2) AS percentage 
-FROM Register  
-GROUP BY contest_id 
+SELECT contest_id, ROUND(COUNT(DISTINCT r.user_id)/(SELECT COUNT(DISTINCT user_id) FROM Users)*100,2) AS percentage 
+FROM Users u
+JOIN Register r
+    USING(user_id)
+GROUP BY contest_id
 ORDER BY percentage DESC, contest_id;
