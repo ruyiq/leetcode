4a836160c328a0b6c82e713cc1c51c44984127e4
Ruyi Qu <ruyicassiel@gmail.com>
2022-09-26 23:21:10 -0400

    Time: 351 ms (26.61%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,8 +1,8 @@
-WITH cte AS (
-    SELECT machine_id, process_id, (MAX(timestamp)-MIN(timestamp)) AS diff
-    FROM Activity
-    GROUP BY machine_id, process_id  
-    )
+with cte as (
+    SELECT machine_id,process_id, SUM(IF(activity_type="end",timestamp,0)-IF(activity_type="start",timestamp,0)) AS diff 
+    FROM Activity 
+    GROUP BY machine_id, process_id
+)
 
 SELECT machine_id,
     ROUND(AVG(diff),3) processing_time 
