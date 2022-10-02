bab5f663c4f6846de0b92fca8cabb16b6bd9ac66
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 17:51:59 -0400

    Time: 869 ms (37.13%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,4 +1,3 @@
 SELECT user_id, time_stamp AS last_stamp
 FROM (SELECT *,rank() over (PARTITION BY user_id ORDER BY time_stamp DESC) ranking FROM Logins WHERE LEFT(time_stamp,4)="2020") as s
 WHERE s.ranking=1
-GROUP BY user_id 
