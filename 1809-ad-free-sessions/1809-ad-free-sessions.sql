1803c559fd1e8444da0cbbea54845133e877639c
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 17:22:26 -0400

    Time: 1004 ms (57.38%), Space: 0B (100.00%) - LeetHub

--- 1809-ad-free-sessions/1809-ad-free-sessions.sql
+++ 1809-ad-free-sessions/1809-ad-free-sessions.sql
@@ -1,5 +1,7 @@
-SELECT p.session_id
+SELECT session_id
 FROM Playback p
 LEFT JOIN Ads a
-    ON a.customer_id=p.customer_id AND timestamp>=start_time AND timestamp<=end_time
-WHERE a.ad_id IS NULL;
+    ON p.customer_id=a.customer_id
+        AND p.start_time <= a.timestamp
+        AND p.end_time >= a.timestamp
+WHERE ad_id IS NULL
