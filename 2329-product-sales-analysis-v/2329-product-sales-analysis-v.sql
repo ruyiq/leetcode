2ad60ff2e747b2cd8751ebce1088983e2948c4c5
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 22:19:27 -0400

    Time: 1639 ms (10.69%), Space: 0B (100.00%) - LeetHub

--- 2329-product-sales-analysis-v/2329-product-sales-analysis-v.sql
+++ 2329-product-sales-analysis-v/2329-product-sales-analysis-v.sql
@@ -1,6 +1,6 @@
-SELECT user_id, SUM(s.quantity*p.price) AS spending
+SELECT s.user_id, SUM(s.quantity*p.price) AS spending
 FROM Sales s
 JOIN Product p
     USING(product_id)
-GROUP BY user_id
+GROUP BY s.user_id
 ORDER BY spending DESC, user_id;
