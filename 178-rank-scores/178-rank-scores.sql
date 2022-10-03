355b022fc64bf3b61c6a934fb2fc067bb8e68928
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 22:54:11 -0400

    Time: 560 ms (25.38%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,2 +1,2 @@
-SELECT score, s.rank
-FROM (SELECT dense_rank() over (ORDER BY score DESC) AS 'rank', score FROM Scores) as s;
+SELECT score, ranking AS "rank"
+FROM (SELECT *, dense_rank() over (ORDER BY score DESC) ranking FROM Scores) s;
