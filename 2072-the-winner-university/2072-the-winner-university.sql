41b2c39a235a784b0250304c59b09658119363af
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 22:03:59 -0400

    Time: 1106 ms (40.65%), Space: 0B (100.00%) - LeetHub

--- 2072-the-winner-university/2072-the-winner-university.sql
+++ 2072-the-winner-university/2072-the-winner-university.sql
@@ -1,13 +1,14 @@
-WITH cte1 AS (
-    SELECT SUM(IF(score>=90,1,0)) as total
-    FROM NewYork 
-),
-    cte2 AS (
-    SELECT SUM(IF(score>=90,1,0)) as total
+with cte1 as (
+    SELECT COUNT(score) as c
+    FROM NewYork
+    WHERE score>=90
+), cte2 as (
+    SELECT COUNT(score) as c
     FROM California 
-    )
+    WHERE score>=90
+)
 
-SELECT CASE WHEN cte1.total>cte2.total THEN "New York University"
-            WHEN cte1.total<cte2.total THEN "California University"
-            ELSE "No Winner" END AS winner    
-FROM cte1, cte2;
+SELECT CASE WHEN cte1.c>cte2.c THEN "New York University"
+            WHEN cte1.c<cte2.c THEN "California University"
+            ELSE "No Winner" END AS winner
+FROM cte1, cte2;              
