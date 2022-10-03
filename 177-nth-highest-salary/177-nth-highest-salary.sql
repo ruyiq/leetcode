ca2bc1b20655f3d8af1989c3173026809fcbb705
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 22:49:37 -0400

    Time: 667 ms (21.23%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -1,11 +1,10 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-      SELECT CASE WHEN COUNT(*)>0 THEN s.salary 
-                ELSE null END AS getNthHighestSalary
-      FROM (SELECT dense_rank() over (ORDER BY salary DESC) AS ranking, salary FROM Employee) s
-      WHERE s.ranking = N
-     
-      );
+      SELECT CASE WHEN COUNT(salary)=0 THEN null
+        ELSE salary END AS getNthHighestSalary
+      FROM (SELECT *, dense_rank() over (ORDER BY salary DESC) ranking FROM Employee) s
+      WHERE ranking=N
       
+  );
 END
