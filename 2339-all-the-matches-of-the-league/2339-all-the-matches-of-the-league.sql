e036031e4f7f13cc1ce380cf4641dc0906a3c167
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-02 22:24:04 -0400

    Time: 1312 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.sql
+++ 2339-all-the-matches-of-the-league/2339-all-the-matches-of-the-league.sql
@@ -1,3 +1,5 @@
-SELECT t1.team_name AS home_team, t2.team_name AS away_team  
-FROM Teams t1, Teams t2
-WHERE t1.team_name != t2.team_name;
+SELECT t1.team_name AS "home_team" ,t2.team_name  AS "away_team" 
+FROM Teams t1
+CROSS JOIN Teams t2
+WHERE t1.team_name <> t2.team_name
+GROUP BY t1.team_name, t2.team_name
