71eca858b2892afbc630fb723d3d3a3b830a9d38
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-08 13:45:29 -0400

    Time: 624 ms (48.10%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,3 +1,6 @@
-SELECT DISTINCT (l1.num) AS ConsecutiveNums 
+SELECT DISTINCT l1.num AS ConsecutiveNums 
 FROM Logs l1, Logs l2, Logs l3
-WHERE l1.id+1= l2.id AND l1.id +2= l3.id AND l1.num=l2.num AND l2.num=l3.num;
+WHERE l1.id+1=l2.id 
+    AND l2.id+1=l3.id
+    AND l1.num = l2.num
+    AND l2.num = l3.num;
