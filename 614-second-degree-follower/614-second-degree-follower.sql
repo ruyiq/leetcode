6fdf43577aa5d056c8180eac28824db16d6f0ced
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-09 19:34:18 -0400

    Time: 483 ms (60.25%), Space: 0B (100.00%) - LeetHub

--- 614-second-degree-follower/614-second-degree-follower.sql
+++ 614-second-degree-follower/614-second-degree-follower.sql
@@ -1,5 +1,18 @@
-SELECT followee AS follower, COUNT(followee) AS num
-FROM Follow
-WHERE followee IN (SELECT follower FROM Follow)
-GROUP BY followee
-ORDER BY follower;
+with cte1 as (
+    SELECT followee, COUNT(follower) as c
+    FROM Follow
+    GROUP BY followee
+    HAVING COUNT(follower)>=1
+), cte2 as (
+    SELECT follower
+    FROM FOllow
+    GROUP BY follower
+    HAVING COUNT(follower)>=1
+)
+
+SELECT cte2.follower AS follower , cte1.c AS num 
+FROM cte1
+JOIN cte2
+    ON cte1.followee=cte2.follower
+ORDER BY cte2.follower
+    
