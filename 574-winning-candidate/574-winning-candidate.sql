51c6e6296c5a549ec55dcec6f0bc774aa913721a
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-09 13:51:57 -0400

    Time: 502 ms (60.22%), Space: 0B (100.00%) - LeetHub

--- 574-winning-candidate/574-winning-candidate.sql
+++ 574-winning-candidate/574-winning-candidate.sql
@@ -1,11 +1,12 @@
-with cte as (
-    SELECT candidateId, COUNT(candidateId) as count_c
+with cte as(
+    SELECT candidateId, COUNT(DISTINCT id) as c
     FROM Vote
     GROUP BY candidateId
-    ORDER BY count_c DESC 
+    ORDER BY c DESC
     LIMIT 1
 )
 
-SELECT c.name 
-FROM Candidate c,cte
-WHERE c.id=cte.candidateId;
+SELECT c1.name
+FROM Candidate c1
+JOIN cte c2
+    ON c2.candidateId = c1.id;
