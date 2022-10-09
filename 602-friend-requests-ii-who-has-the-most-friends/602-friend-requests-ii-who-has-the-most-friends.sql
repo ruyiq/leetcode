c35f3bcd9217095d521343d9bade3e6e704f547d
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-09 15:15:25 -0400

    Time: 377 ms (38.92%), Space: 0B (100.00%) - LeetHub

--- 602-friend-requests-ii-who-has-the-most-friends/602-friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,18 +1,16 @@
-WITH cte AS
-(
-SELECT
-    requester_id AS user,
-    accepter_id  AS friend
-FROM RequestAccepted
-    UNION ALL
-SELECT
-    accepter_id AS user,
-    requester_id  AS friend
-FROM RequestAccepted
+with cte as (
+    SELECT
+        requester_id AS user,
+        accepter_id  AS friend
+    FROM RequestAccepted
+        UNION ALL
+    SELECT
+        accepter_id AS user,
+        requester_id  AS friend
+    FROM RequestAccepted
 )
-
-SELECT user AS id, COUNT(friend) AS num
+SELECT user AS id, COUNT(user) AS num
 FROM cte
 GROUP BY id
-ORDER BY num DESC
-LIMIT 1;
+ORDER BY COUNT(user) DESC
+LIMIT 1
