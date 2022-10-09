4dcc130c3a0d54788cc11de33e94e487bffbfab2
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-09 13:47:01 -0400

    Time: 441 ms (38.50%), Space: 0B (100.00%) - LeetHub

--- 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,5 @@
-WITH cte AS (
-    SELECT managerId, COUNT(managerId) AS count_m
-    FROM Employee
-    GROUP BY managerId
-)
-
-SELECT name
-FROM Employee e
-JOIN cte c
-    ON e.id=c.managerId AND count_m>=5;
+SELECT m.name
+FROM Employee e1, Employee m
+WHERE e1.managerId=m.id
+GROUP BY m.id
+HAVING COUNT(DISTINCT e1.id)>=5;
