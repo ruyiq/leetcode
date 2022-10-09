5d07ed18381a0dbbe7e6d56004eaea6a460e5b4c
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-09 13:19:55 -0400

    Time: 410 ms (40.93%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,14 +1,14 @@
+-- create select statement:
 SELECT
-    (CASE
-        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
-        WHEN MOD(id, 2) != 0 AND counts = id THEN id
-        ELSE id - 1
-    END) AS id,
+    -- check if number is last number:
+    IF(id < (SELECT MAX(id) FROM Seat),
+      -- if not last number, then check logical condition:
+      IF(MOD(id,2) = 0, id-1,id+1),
+       -- if is last number, check if odd or even, otherwise return id:
+        IF(MOD(id,2) = 0, id-1,id)
+       ) AS id,
     student
-FROM
-    seat,
-    (SELECT
-        COUNT(*) AS counts
-    FROM
-        seat) AS seat_counts
-ORDER BY id ASC;
+-- from clause:
+FROM Seat
+-- order by id ASC:
+ORDER BY id ASC
