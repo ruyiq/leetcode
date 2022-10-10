93e4feb71b19f0d3287f0c1f147808d37bd0b937
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-10 14:46:54 -0400

    Time: 1538 ms (17.45%), Space: 0B (100.00%) - LeetHub

--- 1126-active-businesses/1126-active-businesses.sql
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -1,13 +1,13 @@
-with by_event as (
-    SELECT event_type, sum(occurences)/count(occurences) as avg
+with avg_activity as (
+    SELECT event_type, AVG(occurences) as o
     FROM Events
     GROUP BY event_type
 )
 
-SELECT business_id
-FROM Events
-JOIN by_event b
-    USING(event_type)
-WHERE occurences>b.avg
-GROUP BY business_id
-HAVING COUNT(*) > 1
+SELECT e.business_id
+FROM Events e
+JOIN avg_activity a
+    ON e.event_type = a.event_type 
+WHERE e.occurences > a.o
+GROUP BY e.business_id
+HAVING COUNT(e.event_type)>1;
