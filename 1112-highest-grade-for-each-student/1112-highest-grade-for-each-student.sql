607d9cf8da75aaaffc6ecd04b1f396a1f728f1b6
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-10 14:36:58 -0400

    Time: 957 ms (22.40%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,16 +1,3 @@
-SELECT student_id, 
-course_id, 
-grade
-
-FROM (SELECT e.*, 
-      RANK() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id ASC) AS rnk 
-	  FROM enrollments e) x
-	 
-WHERE x.rnk = 1
-ORDER BY student_id ASC
-
-# select the three columns we need for output
-#sub query and rank window function to rank the grades from highest to lowest
-# order the grades biggest to smallest and then course id smallest to biggest incase there is a tie
-#filter where the grade rank is 1 (highest grade)
-#ordered based on what the question asked
+SELECT student_id, course_id, grade
+FROM (SELECT *, DENSE_RANK() OVER (PARTITION BY student_id ORDER BY grade DESC, course_id) rk FROM Enrollments) s
+WHERE rk=1
