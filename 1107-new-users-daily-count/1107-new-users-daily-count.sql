224d4fcca0f052b1030b9276a9e3447ef0999adf
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-10 14:30:39 -0400

    Time: 683 ms (56.59%), Space: 0B (100.00%) - LeetHub

--- 1107-new-users-daily-count/1107-new-users-daily-count.sql
+++ 1107-new-users-daily-count/1107-new-users-daily-count.sql
@@ -1,16 +1,10 @@
-with base as
-(
-    select
-        user_id,
-        activity_date,
-        rank() over (partition by user_id order by activity_date) as rk
-    from Traffic
-    where activity = 'login'
+with cte as (
+    SELECT DISTINCT user_id, activity_date
+    FROM (SELECT *, RANK() OVER(PARTITION BY user_id ORDER BY activity_date) ranking FROM Traffic WHERE activity="login") s
+    WHERE ranking=1 
 )
 
-select
-    activity_date as login_date,
-    count(distinct user_id) as user_count
-from base
-where rk = 1 and activity_date between date_sub('2019-06-30', interval 90 day) and '2019-06-30'
-group by activity_date
+SELECT activity_date AS login_date, COUNT(user_id) AS user_count  
+FROM cte
+WHERE activity_date between date_sub('2019-06-30', interval 90 day) and '2019-06-30'
+GROUP BY activity_date;
