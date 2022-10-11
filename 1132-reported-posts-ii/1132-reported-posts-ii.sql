504e57a0c17f1136f616a8e59dd39f3645593595
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-11 12:16:48 -0400

    Time: 1137 ms (68.00%), Space: 0B (100.00%) - LeetHub

--- 1132-reported-posts-ii/1132-reported-posts-ii.sql
+++ 1132-reported-posts-ii/1132-reported-posts-ii.sql
@@ -1,23 +1,11 @@
-WITH spam AS
-(
-SELECT action_date, count(distinct a.post_id) AS spam_cnt
-FROM Actions a
-#LEFT JOIN Removals r ON a.post_id = r.post_id
-WHERE action = "report" AND extra = "spam"
-GROUP BY action_date
+with cte as (
+    SELECT post_id, COUNT(distinct r.post_id)/COUNT(distinct a.post_id) as p
+    FROM Actions a
+    LEFT JOIN Removals r
+        USING(post_id)
+    WHERE action="report" AND extra="spam"
+    GROUP BY action_date
 )
-, remov AS
-(
-SELECT a.action_date, count(distinct r.post_id) AS remov_cnt
-FROM Actions a
-LEFT JOIN Removals r ON a.post_id = r.post_id
-WHERE action = "report" AND extra = "spam"
-GROUP BY action_date
-)
-, fraction AS
-(SELECT s.action_date, (remov_cnt/spam_cnt)*100 AS pct
-FROM spam s JOIN remov r
-ON s.action_date = r.action_date
-)
-SELECT ROUND(AVG(pct),2) AS average_daily_percent
-FROM fraction;
+
+SELECT ROUND(avg(p)*100,2) AS average_daily_percent 
+FROM cte;
