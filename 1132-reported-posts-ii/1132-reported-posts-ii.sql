a46d84902885431571cb7ebf9e59b8f48ab6b8f0
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-11 12:31:35 -0400

    Time: 1357 ms (40.66%), Space: 0B (100.00%) - LeetHub

--- 1132-reported-posts-ii/1132-reported-posts-ii.sql
+++ 1132-reported-posts-ii/1132-reported-posts-ii.sql
@@ -1,5 +1,5 @@
-with cte as (
-    SELECT post_id, COUNT(distinct r.post_id)/COUNT(distinct a.post_id) as p
+WITH cte AS (
+    SELECT COUNT(distinct r.post_id)/COUNT(distinct a.post_id) as c
     FROM Actions a
     LEFT JOIN Removals r
         USING(post_id)
@@ -7,5 +7,5 @@ with cte as (
     GROUP BY action_date
 )
 
-SELECT ROUND(avg(p)*100,2) AS average_daily_percent 
+SELECT ROUND(avg(c)*100,2) AS average_daily_percent 
 FROM cte;
