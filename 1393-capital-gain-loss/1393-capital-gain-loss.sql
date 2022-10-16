acdc5989734c6ebae20ec70c4cbf9f021ad660f6
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-16 18:42:24 -0400

    Time: 1410 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,3 +1,16 @@
-SELECT stock_name, SUM(IF (operation='Sell', price, 0))-SUM(IF (operation='Buy', price, 0)) AS capital_gain_loss 
-FROM Stocks
-GROUP BY stock_name;
+with buy as (
+    SELECT stock_name, SUM(price) as buy
+    FROM Stocks
+    WHERE operation = "buy"
+    GROUP BY stock_name
+), sell as (
+    SELECT stock_name, SUM(price) as sell
+    FROM Stocks
+    WHERE operation = "sell"
+    GROUP BY stock_name
+)
+
+SELECT s.stock_name, s.sell-b.buy AS capital_gain_loss 
+FROM buy b
+JOIN sell s
+    USING (stock_name);
