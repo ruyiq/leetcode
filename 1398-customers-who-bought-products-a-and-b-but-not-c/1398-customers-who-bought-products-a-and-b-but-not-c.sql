32691528d2dd9399dde41430842d5f3af9879aef
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-16 18:50:43 -0400

    Time: 750 ms (49.95%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,8 +1,17 @@
-SELECT customer_id, customer_name
-FROM Customers
-LEFT JOIN Orders
-USING(customer_id)
-GROUP BY (customer_id)
-HAVING SUM(product_name='A') > 0
-AND SUM(product_name='B') > 0
-AND SUM(product_name='C') = 0
+with cust as(
+    SELECT *
+    FROM Orders o
+    WHERE customer_id NOT IN (SELECT customer_id FROM Orders WHERE product_name="C")
+), cte2 as (
+    SELECT c1.customer_id  
+    FROM cust c1, cust c2
+    WHERE c1.customer_id = c2.customer_id
+        AND c1.product_name="A"
+        AND c2.product_name="B"
+    GROUP BY customer_id
+)
+
+SELECT cte2.customer_id, c.customer_name
+FROM cte2
+JOIN Customers c
+    USING(customer_id);
