dd1ad349059221e6112e26968d97d739c744f664
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-16 19:06:00 -0400

    Time: 607 ms (17.37%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,3 +1,11 @@
-SELECT a.sale_date, a.sold_num-o.sold_num AS diff
-FROM Sales a, Sales o
-WHERE a.sale_date=o.sale_date AND  a.fruit="apples" AND o.fruit="oranges"
+with cte as (
+    SELECT sale_date,
+        SUM(IF(fruit="apples", sold_num, 0)) AS apples,
+        SUM(IF(fruit="oranges", sold_num, 0)) AS oranges    
+    FROM Sales
+    GROUP BY sale_date
+)
+
+SELECT sale_date, apples-oranges as diff         
+FROM cte
+ORDER BY sale_date;
