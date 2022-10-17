0b09a751b7dd0ed3feefcac9592bda509839ef41
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-17 14:59:28 -0400

    Time: 1346 ms (5.64%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,8 +1,6 @@
-SELECT CASE WHEN from_id<to_id THEN from_id
-            ELSE to_id END AS person1,
-       CASE WHEN from_id<to_id THEN to_id
-            ELSE from_id END AS person2,
-       COUNT(duration) AS call_count,
-       SUM(duration) AS total_duration
+SELECT LEAST(from_id, to_id) AS person1, 
+    GREATEST(from_id, to_id) AS person2, 
+    COUNT(*) AS call_count,
+    SUM(duration) AS total_duration
 FROM Calls
 GROUP BY person1, person2;
