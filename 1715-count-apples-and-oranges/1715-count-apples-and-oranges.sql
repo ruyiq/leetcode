5cc0916012c30b008785f0e66917371549ec3975
Ruyi Qu <ruyicassiel@gmail.com>
2022-10-18 15:46:10 -0400

    Time: 1932 ms (10.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
@@ -0,0 +1,4 @@
+SELECT SUM(b.apple_count+IFNULL(c.apple_count,0)) AS apple_count ,                                    SUM(b.orange_count+IFNULL(c.orange_count,0)) AS orange_count 
+FROM Boxes b
+LEFT JOIN Chests c
+    USING(chest_id)
