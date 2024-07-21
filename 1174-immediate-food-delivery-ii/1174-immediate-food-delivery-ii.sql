00e1ce61afb70b4c02f554576c6d90143d1792dc
Ruyi Qu <ruyicassiel@gmail.com>
2024-07-21 00:05:52 -0400

    Time: 892 ms (31.18%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,8 +1,10 @@
-WITH cte AS(
-    SELECT * 
-    FROM (SELECT *, rank() over(PARTITION BY customer_id ORDER BY order_date) rk FROM Delivery) s
-    WHERE rk=1)
+# Write your MySQL query statement below
+WITH labeled_orders AS (
+    SELECT *, 
+    RANK() OVER (PARTITION BY customer_id ORDER BY order_date) as orders_rank
+    FROM Delivery
+)
 
-SELECT ROUND(COUNT(delivery_id)/(SELECT COUNT(delivery_id) FROM cte)*100,2) AS immediate_percentage 
-FROM cte
-WHERE order_date= customer_pref_delivery_date;
+SELECT  ROUND(SUM(IF(order_date=customer_pref_delivery_date,1,0))*100/COUNT(delivery_id),2) immediate_percentage 
+FROM labeled_orders 
+WHERE orders_rank=1
