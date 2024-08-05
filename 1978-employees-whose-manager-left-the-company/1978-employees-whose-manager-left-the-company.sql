5d83e65f4079194cbc27d7fd5fa7ae37e84d8c7f
Ruyi Qu <ruyicassiel@gmail.com>
2024-08-04 20:11:50 -0400

    Time: 364 ms (23.84%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,5 +1,12 @@
-SELECT employee_id 
-FROM Employees
-WHERE salary<30000 
-    AND manager_id NOT IN (SELECT employee_id FROM Employees)
-ORDER BY employee_id;
+# Write your MySQL query statement below
+WITH less_than_3000 AS (
+    SELECT employee_id, manager_id
+    FROM Employees
+    GROUP BY employee_id
+    HAVING SUM(salary)< 30000
+)
+
+SELECT employee_id
+FROM less_than_3000 
+WHERE manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id
