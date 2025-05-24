1368123b1a3d0ac95c47ac436c59c6e2709fb18d
Ruyi Qu <ruyicassiel@gmail.com>
2025-05-24 19:59:14 -0400

    Time: 543 ms (78.42%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,5 +1,21 @@
-SELECT ROUND(COUNT(DISTINCT player_id )/(SELECT COUNT(DISTINCT player_id) FROM activity),2) as fraction
-FROM (SELECT *, 
-      MIN(event_date) OVER(PARTITION BY player_id ORDER BY event_date) first_log 
-      FROM Activity) s
-WHERE datediff(event_date,first_log) = 1
+# Write your MySQL query statement below
+WITH first_date AS (
+    SELECT player_id, MIN(event_date) AS first_day
+    FROM Activity
+    GROUP BY player_id
+),
+
+returned_players AS(
+    SELECT DISTINCT a.player_id
+    FROM Activity a
+    JOIN first_date f USING(player_id)
+    WHERE DATEDIFF(a.event_date, f.first_day)=1
+),
+
+all_players AS (
+    SELECT COUNT(DISTINCT player_id) as cnt
+    FROM Activity
+)
+
+SELECT ROUND((SELECT COUNT(*) FROM returned_players)/all_players.cnt,2) AS fraction  
+FROM all_players
