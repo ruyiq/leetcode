4a07699b8c828651a8102376c519794413efa610
Ruyi Qu <ruyicassiel@gmail.com>
2025-05-24 22:24:26 -0400

    Time: 877 ms (95.47%), Space: 0B (100%) - LeetHub

--- 1132-reported-posts-ii/1132-reported-posts-ii.sql
+++ 1132-reported-posts-ii/1132-reported-posts-ii.sql
@@ -1,11 +1,25 @@
-WITH cte AS (
-    SELECT COUNT(distinct r.post_id)/COUNT(distinct a.post_id) as c
-    FROM Actions a
-    LEFT JOIN Removals r
-        USING(post_id)
-    WHERE action="report" AND extra="spam"
+-- for each date, which posts are spam
+-- for much of these posts where later removed
+
+WITH all_spam AS (
+    SELECT DISTINCT action_date, post_id
+    FROM Actions
+    WHERE extra = "spam"    
+),
+
+removed AS (
+    SELECT action_date, 
+        SUM(CASE
+            WHEN post_id IN (SELECT post_id FROM Removals) THEN 1
+            ELSE 0 END) AS removed_cnt
+    FROM all_spam
     GROUP BY action_date
 )
 
-SELECT ROUND(avg(c)*100,2) AS average_daily_percent 
-FROM cte;
+SELECT ROUND(AVG(percentage)*100,2) AS average_daily_percent 
+FROM (
+    SELECT a.action_date, removed_cnt/ COUNT(post_id) AS percentage
+    FROM all_spam a
+    LEFT JOIN removed r USING(action_date)
+    GROUP BY a.action_date
+) temp
